8b0106a6d4978f54814c13463716c0712ed1cc25
poporeki <yansk@live.com>
2018-08-23 02:56:39 +0800

    '823'

--- router/back/article_update.js
+++ router/back/article_update.js
@@ -7,7 +7,7 @@ var arcTagMod = require("../../modules/Article/articleTag");
 
 var scriptlink = require("./arclist_script");
 /* 修改文章 */
-router.get("/:artid", function(req, res) {
+router.get("/:artid", function (req, res, next) {
   let renObj = {
     pageTitle: "修改文章",
     submitURL: "/backend/art/updatearticle/" + req.params.artid,
@@ -19,7 +19,7 @@ router.get("/:artid", function(req, res) {
   /* 获取文章信息 */
   let getArcInfo = () => {
     return new Promise((resolve, reject) => {
-      arcMod.showOneArticle(req.params.artid, function(err, result) {
+      arcMod.showOneArticle(req.params.artid, function (err, result) {
         if (err) reject(err);
         if (result.length === 0) reject("not article");
         let arc = result[0];
@@ -38,7 +38,7 @@ router.get("/:artid", function(req, res) {
   /* 获取文章分类 */
   let getArcType = () => {
     return new Promise((resolve, reject) => {
-      arcTypeMod.findArticleType({}, function(err, result) {
+      arcTypeMod.findArticleType({}, function (err, result) {
         if (err) reject(err);
         renObj["typeName"] = result;
         resolve();
@@ -49,13 +49,13 @@ router.get("/:artid", function(req, res) {
   let getArcTags = () => {
     return new Promise(
       (resolve,
-      reject => {
-        arcTagMod.findArticleTags({}, function(err, result) {
-          if (err) reject(err);
-          renObj["tagName"] = result;
-          resolve();
-        });
-      })
+        reject => {
+          arcTagMod.findArticleTags({}, function (err, result) {
+            if (err) reject(err);
+            renObj["tagName"] = result;
+            resolve();
+          });
+        })
     );
   };
   getArcInfo()
@@ -67,8 +67,8 @@ router.get("/:artid", function(req, res) {
     .catch(() => next(err));
 });
 /* 修改文章 */
-router.post("/:artid", function(req, res) {
-  arcMod.updateArticle(req, function(err, result) {
+router.post("/:artid", function (req, res) {
+  arcMod.updateArticle(req, function (err, result) {
     if (err) {
       console.log(err);
       return;
