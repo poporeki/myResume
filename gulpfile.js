bba971ab3aa08e3d9692df7e33d944f468b5bb29
poporeki <yansk@live.com>
2019-02-04 14:36:34 +0800

    190204

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,45 @@
-var gulp = require('gulp');
-var browSync = require('browser-sync').create();
-var reload = browSync.reload;
-var sass = require('gulp-sass');
-var PORT = require('./app');
+const {src,dest,watch,series,parallel}=require('gulp');
+const sass=require('gulp-sass');
+const browsync=require('browser-sync').create();
+const nodemon=require('gulp-nodemon');
+reload=browsync.reload;
+
+
 var path = {
     js: './public/**/*.js',
     ejs: './views/**/*.ejs',
-    scss: './public/scss/**/*.scss'
+    scss: './public/scss/**/*.scss',
+    output:{
+        css:'./public/css'
+    }
 }
 
-gulp.task('sass', function () {
-    return gulp.src(path.scss)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'))
-        .pipe(reload({
-            stream: true
-        }));;
-})
-gulp.task('brow-sync', ['sass'], function () {
-    browSync.init({
-        proxy: 'http://localhost:3000',
-        notify: false,
-        port: 2002
-    })
-    gulp.watch(path.scss, ['sass']);
-    gulp.watch([path.ejs, path.js, 'app.js']).on("change", reload);
-})
+function defaultTask(cb){
+    watch(path.scss, sassTask);
+    watch([path.ejs, path.js, 'app.js']).on("change", reload);
+}
+function javascript(){
 
-gulp.task('default', ['brow-sync']);
-gulp.task('wsass', function () {
-    gulp.watch(path.scss, ['sass']);
-});
+}
+function nodemonTask(){
+    nodemon({
+        script:'app.js',
+        ignore:['node_modules/']
+    }).on('start',function(){
+        browserSync();
+    })
+}
+function browserSync(){
+        browsync.init({
+            proxy: 'http://localhost:3000',
+            port:3001
+        });
+        defaultTask();
+}
+function sassTask(){
+    return src(path.scss)
+    .pipe(sass().on('error',sass.logError))
+    .pipe(dest(path.output.css))
+    .pipe(browsync.stream())
+}
+exports.default=series(sassTask,nodemonTask,defaultTask);
