7157b1be0598a80d0a51dcb864ef0dd9c2167b4e
poporeki <yansk@live.com>
2019-05-01 13:16:07 +0800

    '190501'

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ app.use(express.static(path.join(__dirname, "./public")));
 app.use("v", express.static(path.join(__dirname, "./dist")));
 //设置跨域访问
 app.use(function (req, res, next) {
+  console.log(req.headers.origin || req.headers.host);
   res.header("Access-Control-Allow-Origin", req.headers.origin || req.headers.host);
   res.header("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept");
   // res.header("Access-Control-Allow-Headers", "X-Requested-With");
